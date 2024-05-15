7335fa3698e1935c55b1513c50157e9436fd4f3d
Demir <79748534+demirCann@users.noreply.github.com>
2024-05-15 23:32:44 +0300

    İnitial commit

---
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.foodapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoritesFragment
+            )
+        )
+
+
+
+    }
+}
