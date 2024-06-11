9e2e993bbc709c7e2d26df482dd42c096ade3f50
Demir <79748534+demirCann@users.noreply.github.com>
2024-06-12 01:32:02 +0300

    Updated theme colors and added new strings for buttons in the RecipeFinder app.

--- app/src/main/java/com/demircandemir/reciper/ui/theme/Theme.kt
+++ app/src/main/java/com/demircandemir/reciper/ui/theme/Theme.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -19,17 +20,15 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    tertiary = Pink40,
+    background = Color(0xFF2F2D2F),
+    surface = Color(0xFF2F2D2F),
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
+    onBackground = Color(0xFF2F2D2F),
     onSurface = Color(0xFF1C1B1F),
-    */
+
 )
 
 @Composable
